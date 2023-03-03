23c098a0ead97828d0318c7b57308cb81514fb2d
Aayush Singh <97450185+Progranner1234@users.noreply.github.com>
2023-03-03 15:06:23 +0530

    final

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -3,8 +3,6 @@ const recipe_routes = express()
 const cookieParser  = require('cookie-parser')
 recipe_routes.use(cookieParser())
 const recipeController = require('../controllers/recipeController')
-const userMiddleware = require('../middleware/userMiddleware')
-const limitApiAccess = require('../middleware/limitApiAccess')
 
 recipe_routes.post('/recipe',recipeController.getRecipe)
 recipe_routes.post('/get-nutrients',recipeController.getNutritionValue)
